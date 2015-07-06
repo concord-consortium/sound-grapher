dfd58ec258f9311b556052c77509edcfb45a84fe
Aaron Unger <aunger@concord.org>
2015-07-06 14:52:00 -0700

    Better messages in the error dialog

--- js/error_dialog.js
+++ js/error_dialog.js
@@ -15,10 +15,31 @@
   };
 
   ErrorDialog.prototype._appendDetails = function() {
+    var message, alternatives;
+    if (bowser.msie) {
+      message = "The sound grapher will not work in Internet Explorer at this time.";
+      alternatives = "Please try this in Chrome or Firefox.";
+    } else if (bowser.safari) {
+      message = "The sound grapher will not work in Safari at this time.";
+      alternatives = "Please try this in Chrome or Firefox.";
+    } else if (bowser.ios) {
+      var device = bowser.iphone ? 'the iPhone' : (bowser.ipad ? "the iPad" : "iOS");
+      message = "The sound grapher will not work on " + device + " at this time.";
+      alternatives = "Please try this in Chrome or Firefox on a desktop or laptop.";
+    } else if (bowser.chrome || bowser.firefox) {
+      message = "The sound grapher will not work without microphone input.";
+      alternatives = "Please click 'Allow' in the microphone settings to enable microphone access for this site.";
+    } else if (bowser.android) {
+      message = "The sound grapher will not work in the Android browser at this time.";
+      alternatives = "Please try this in Chrome for Android, or in Chrome or Firefox on a desktop or laptop.";
+    } else {
+      message = "The sound grapher will not work in your browser at this time.";
+      alternatives = "Please try this in Chrome or Firefox on a desktop or laptop.";
+    }
+
     $('.dialog').append(
-      '<div>'+
-      'Your browser doesn\'t support microphone input. Please switch to a browser which does.'+
-      '</div>'
+      '<div>'+message+'</div>'+
+      '<div>'+alternatives+'</div>'
     );
   };
 
